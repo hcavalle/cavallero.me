75da52f37d7c6b482cb1625bd1f9b0bcb1b335ab
root <root@fbox.(none)>
2013-01-30 12:04:08 -0800

    initial proj version

---
+++ index.php
@@ -0,0 +1,154 @@
+<?php 
+	$title = 'harrison cavallero';
+	$description = "harrison cavallero. tech ideation impact";
+	$keywords = "harrison cavallero, web development, web design, IT, tech, socent global, social enterprise";
+	include("head_include.php"); 
+?>	
+<body>
+	<header>
+		<!--<a href="resume.pdf" target="blank"><h1 name="title" class=" title gigantic"> <span class="accentColor">h</span>arrison <span class="accentColor">cav</span>allero </h1></a> -->
+		<a href="index.php" class="logo" ><img class = "logo" src ="images/hcav-logo.png" alt="harrison cavallero"></a> 
+	<!--<div class="hidden" position ="relative" width="100%" align="center" style="margin-top: 30px; margin-bottom: 100px">
+		<a class="logo" href="interactiveresume.pdf"><img class = "logo"src ="images/harrisoncavallero.gif" alt="harrison cavallero"></a> 
+		
+		<p class="comingsoon">in progress</p> 
+	</div>-->
+	
+	</header>
+	<div class="wrapper">
+			<nav class="darknav boxshadow">
+				<div class="innerwrapper">
+					<h3 class = " "><a id="skillsTitle" href="#">skills. </a></h2> <h3 class=" Georgia"> <a id="workTitle" href="#">work.</a></h3> <h3 class=" Georgia"> <a href="interactiveresume.pdf" target="_blank">resume.</a></h3>
+				</div>
+			</nav>
+			
+			<div class="relwrapper boxshadow">
+					<div id="content-home" class="content">
+						<h2 class="large" ><span class="accentColor">{ </span>social good<span class="accentColor"> }</span> </h2>
+						<h2>through web development and technology </h2>
+						<h3>that's what i'm about</h3>
+						<p> more to come, this is a work in progress</p>
+						<div class="innercontent">
+							<div class="relwrapper">
+							</div>
+						</div>
+					</div>
+			</div>
+			<!--<div id="back-top">
+				<a href="#back-top"><span></span>Back to Top</a>
+			</div> -->
+			<div id="recentwork" class="content">
+				<nav class=""><h2 class ="large dashedbottom">recent work</h2></nav>
+				<ol class="textbox" style="width:100%;">
+				
+				<li class="textbox">
+					<a href="http://www.picturehealing.com/mobilesite/index.html" target="_blank">
+						<figure>
+							<img src="images/ph_logo.png" alt="Picturehealing - Mobile site" width="250px" height="50px">
+							<figcaption>
+								picture healing mobile site
+							</figcaption>
+						</figure>
+						<dl>
+							<dt>role:</dt>
+							
+							<dd>frontend dev</dd>
+							
+						</dl>
+						<dl>
+							<dt>code:</dt>
+							
+							<dd>responsive HTML&CSS</dd>
+							
+							<dd>jQuery</dd>
+							
+							<dd>bootstrap</dd>
+							
+						</dl>
+					</a>
+				</li>
+				
+				<li class="textbox">
+					<a href="http://www.solturalanguages.com" target="_blank">
+						<figure>
+							<img src="images/sl_logo.png" alt="Soltura Languages" width="300px" height="50px">
+							<figcaption>
+								Soltura Languages
+							</figcaption>
+						</figure>
+						<dl>
+							<dt>Role:</dt>
+							
+							<dd>Full Stack Dev</dd>
+							
+						</dl>
+						<dl>
+							<dt>Tech:</dt>
+							
+							<dd>HTML&CSS</dd>
+							
+							<dd>jQuery</dd>
+							
+							<dd>php/mysql</dd>
+							
+							<dd>CodeIgniter</dd>
+							
+						</dl>
+					</a>
+				</li>
+				
+				<li class="textbox">
+					<a href="http://www.socent.la" target="_blank" >
+						<figure>
+							<img src="images/se_logo.png" alt="SocEnt.LA - Wordpress" width="170px" height="50px">
+							<figcaption>
+								SocEnt.LA - Wordpress
+							</figcaption>
+						</figure>
+						<dl>
+							<dt>Role:</dt>
+							
+							<dd>CMS Customization</dd>
+							
+						</dl>
+						<dl>
+							<dt>Tech:</dt>
+							
+							<dd>HTML & CSS</dd>
+							
+							<dd>php/mysql</dd>
+							
+							<dd>Wordpress CMS</dd>
+							
+						</dl>
+					</a>
+				</li>
+				
+			</ol>
+				<!--
+				
+				-->
+			</div>
+			
+	<div id="footer"> <!-- position ="relative" width="100%" align="center" style="margin-top: 100px; margin-bottom: 100px"> -->
+		<div class="boxshadow" id="footerAccent">
+				<!--top of page jquery
+					http://webdesignerwall.com/tutorials/animated-scroll-to-top -->
+					
+				<div id="top"><a href "#">Test</a></div>
+			</div>
+		<nav class="footer innerwrapper">
+			<span>designed by hcav</span>
+			<ul>
+				<li ><a class= href= "http://www.linkedin.com/in/harrisoncavallero">Linkedin</a></li>
+				<!--<li><a href="http://about.me/cavalleroharrison">about.me</a></li>-->
+			</ul>
+		</nav>
+	</div>
+		<!--<a href='http://monitor.us/?ref=button2'><img src='http://images.monitor.us/monbadges120-40.png' title='Monitor.us - Free website, server & network monitoring tool' border=0 /></a> -->
+</body>
+
+
+<?php
+	include("footer_include.php"); 
+?>
