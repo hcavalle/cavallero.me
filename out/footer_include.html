6981ddab9786853725c9267d81b1a863a135ff44
hcavalle <harrison.cavallero@gmail.com>
2014-08-02 16:08:07 -0700

    initial commit for docpad version of site

---
+++ out/footer_include.html
@@ -0,0 +1,18 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+<script src="scripts/jq.js"></script>
+<!--Google Analytics Code-->
+<script type=\"text/javascript\">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-35693608-1']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+</script>
+
+</html>
+<!--<a href='http://monitor.us/?ref=button2'><img src='http://images.monitor.us/monbadges120-40.png' title='Monitor.us - Free website, server & network monitoring tool' border=0 /></a> -->
---
+++ out/head_include.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link rel="stylesheet" href="style.css">
+ 	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	<meta http-equiv="Content-Language" content="en-us">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" Content="<?php echo $description?>"> </meta>
+	<meta name="keywords" Content="<?php echo $keywords?>"> </meta>
+	<title><?php echo $title ?></title>
+
+
+</head>
+
+<!--<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+		
+		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+		<title>$title</title>
+	
+		  	<!--Google Analytics Code
+		  /*<script type=\"text/javascript\">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-35693608-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>"
+*/
+-->
